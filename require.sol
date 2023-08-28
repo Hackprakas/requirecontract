e3df795c037731a671911066f70010df3cf72a01
PrakashDev <92098897+Hackprakas@users.noreply.github.com>
2023-08-28 19:40:31 +0530

    Update require.sol

--- require.sol
+++ require.sol
@@ -2,27 +2,19 @@
 pragma solidity ^0.8.0;
 
 contract ErrorExamples {
-    uint256 public value=1;
+    uint256 public age;
 
-    function setValue(uint256 _newValue) public {
-        // Use require to check conditions and revert if they are not met
-        require(_newValue > value, "New value must be greater than current value");
-        
-        value = _newValue;
-    }
-
-    function assertExample(uint256 _input) public pure returns (uint256) {
-        // Use assert to validate internal state, should not be used for input validation
-        uint256 result = _input * 2;
-        assert(result >= _input);
-        return result;
-    }
-
-    function revertExample(uint256 _input) public pure returns (uint256) {
-        if (_input == 0) {
-            // Use revert to explicitly revert the transaction with a custom error message
-            revert("Input must be non-zero");
+    function setValue(uint256 Value) public {
+        if(Value==0){
+            revert("Age should not be zero");
         }
-        return 10 / _input;
+        require(Value >= 18, "your age should be greater than 18");
+        
+        age = Value;
+    }
+    function doubleage() public view returns (uint256) {
+        uint256 result=age*2;
+        assert(result >= 36);
+        return result;
     }
 }
